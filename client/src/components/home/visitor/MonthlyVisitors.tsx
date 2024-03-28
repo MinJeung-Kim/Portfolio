8f5a91619ee0807b0b8a0d2230902bd9c3f1c50a
MinJeung-Kim <focso5@gmail.com>
2024-03-28 17:43:34 +0900

    Fix

--- client/src/components/home/visitor/MonthlyVisitors.tsx
+++ client/src/components/home/visitor/MonthlyVisitors.tsx
@@ -4,19 +4,17 @@ import styles from "./Visitor.module.scss";
 export default function MonthlyVisitors() {
   return (
     <div className={styles.monthly_visitor}>
-      <h2>
-        Number of <br />
-        Monthly Visitors
-      </h2>
-      <div className={styles.currnt_month}>
-        <h1>1.32</h1>
-        <div className={styles.today}>
-          <p>230</p>
-          <ArrowCircle />
+      <h2>Number of Monthly Visitors</h2>
+      <div className={styles.wrap}>
+        <div className={styles.currnt_month}>
+          <h1>1.32</h1>
+          <div className={styles.today}>
+            <p>230</p>
+            <ArrowCircle />
+          </div>
         </div>
+        <p>Current Month</p>
       </div>
-
-      <p>Current Month</p>
     </div>
   );
 }
